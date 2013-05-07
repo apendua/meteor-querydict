1d5b1ecd32cbdd46aeb38c57acc8257bf5545e15
Tomasz Lenarcik <tomek.lenarcik@gmail.com>
2013-05-08 01:52:54 +0200

    turn into smart package

---
+++ package.js
@@ -0,0 +1,12 @@
+
+Package.describe({
+    summary: "Query string tools",
+});
+
+Package.on_use(function (api) {
+    api.use('jquery', 'client');
+    api.use('deps', ['client', 'server']);
+
+    api.add_files('lib/jquery.ba-bbq.js', 'client');
+    api.add_files('lib/querydict.js', ['client', 'server']);
+});
