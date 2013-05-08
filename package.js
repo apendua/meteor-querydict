7e6dd8474d38163cb811480e5f19bab4a71be107
Tomasz Lenarcik <tomek.lenarcik@gmail.com>
2013-05-08 12:26:55 +0200

    sugar -> underscore

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ Package.describe({
 
 Package.on_use(function (api) {
     api.use('jquery', 'client');
-    api.use('deps', ['client', 'server']);
+    api.use(['underscore', 'deps'], ['client', 'server']);
 
     api.add_files('lib/jquery.ba-bbq.js', 'client');
     api.add_files('lib/querydict.js', ['client', 'server']);
